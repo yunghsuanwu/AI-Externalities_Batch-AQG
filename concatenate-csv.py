f2cdfff370285bd9fffbace1e69426cbff60832d
Yung-Hsuan Wu <wu8815@gmail.com>
2026-02-06 18:36:33 +0000

    Created two batches, one rerun and the other today's work

--- concatenate-csv.py
+++ concatenate-csv.py
@@ -5,4 +5,4 @@ df3 = pd.read_csv('batch-tasks-04Feb.csv')
 df4 = pd.read_csv('batch-tasks-05Feb.csv')
 
 df_concat = pd.concat([df2, df3, df4])
-df_concat.to_csv('batch-tasks-06Feb.csv', index=False)
+df_concat.to_csv('batch-tasks-06Feb-rerun.csv', index=False)
